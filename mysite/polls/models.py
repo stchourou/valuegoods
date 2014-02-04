d81a24a720b0c4e63f3353916b823048352cedf6
stchourou <stchourou@ucdavis.edu>
2014-02-04 10:00:57 -0800

    first site commit

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+import hashlib
+import os
+import string
+import datetime
+from django.db import models
+
+URL_MAX_LENGTH = 65535
+
+
+# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return   True    #self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
